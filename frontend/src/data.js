fcf55be89606b153c1fe8cfc5d3013427d1c40b2
krlosLopez <krlosroller@gmail.com>
2021-05-25 20:54:03 -0500

    first commit

---
+++ frontend/src/data.js
@@ -0,0 +1,76 @@
+export default{
+    productos:[
+        {
+        _id: '1',
+        name:'Full Zip Running Shirts',
+        category: 'pulseras negras',
+        image:'/image/product-1.jpg',
+        price:60,
+        brand:'Hiskywin',
+        rating: 4.5,
+        numReviews:10,
+        countInStock: 6,
+
+        },
+        {
+            _id: '1',
+            name:'Full Zip Running Shirts',
+            category: 'pulseras negras',
+            image:'/image/product-1.jpg',
+            price:60,
+            brand:'Hiskywin',
+            rating: 4.5,
+            numReviews:10,
+            countInStock: 6,
+    
+            },  
+            {
+                _id: '1',
+                name:'Full Zip Running Shirts',
+                category: 'pulseras negras',
+                image:'/image/producto-1.jpg',
+                price:60,
+                brand:'Hiskywin',
+                rating: 4.5,
+                numReviews:10,
+                countInStock: 6,
+        
+                },
+                {
+                    _id: '1',
+                    name:'Full Zip Running Shirts',
+                    category: 'pulseras negras',
+                    image:'/image/producto-1.jpg',
+                    price:60,
+                    brand:'Hiskywin',
+                    rating: 4.5,
+                    numReviews:10,
+                    countInStock: 6,
+            
+                    },
+                    {
+                        _id: '1',
+                        name:'Full Zip Running Shirts',
+                        category: 'pulseras negras',
+                        image:'/image/producto-1.jpg',
+                        price:60,
+                        brand:'Hiskywin',
+                        rating: 4.5,
+                        numReviews:10,
+                        countInStock: 6,
+                
+                        },
+                        {
+                            _id: '1',
+                            name:'Full Zip Running Shirts',
+                            category: 'pulseras negras',
+                            image:'/image/producto-1.jpg',
+                            price:60,
+                            brand:'Hiskywin',
+                            rating: 4.5,
+                            numReviews:10,
+                            countInStock: 6,
+                    
+                            },
+    ],
+}; 
