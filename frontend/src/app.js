41b60cc0f8590bc5cb597befa346b145b5348425
krlosLopez <krlosroller@gmail.com>
2021-05-27 18:12:46 -0500

    product result

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,6 +1,20 @@
+import Error404Screen from './srceens/Error404Screen.js';
 import HomeScreen from './srceens/HomeScreen.js';
+import ProductScreen from './srceens/ProductScreen.js';
+import { parseRequestUrl } from './utils.js';
+const routes = {
+  '/': HomeScreen, 
+  '/product/:id': ProductScreen,
+};
 const router = () => {
+  const request = parseRequestUrl();
+  const parseUrl = (request.resourse ? `/${request.resourse}`:`/`) +
+  (request.id ? `/:id`:``) + 
+  (request.verb ? `/${request.verb}`:``);
+  const screen = routes[parseUrl]? routes [parseUrl] : Error404Screen;
+
   const main = document.getElementById('main-container');
-  main.innerHTML = HomeScreen.render();
+  main.innerHTML = screen.render();
 };
 window.addEventListener('load', router);
+window.addEventListener('hashchange',router);
