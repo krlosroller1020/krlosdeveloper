d5d14ba5914d1744d8a973d5f3170acc22cda365
krlosLopez <krlosroller@gmail.com>
2021-05-27 13:26:22 -0500

    home screen

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,6 @@
-import HomeScreen from './screens/HomeScreen.js';
-const router = ()=> {
-    const main = document.getElementById("main-container");
-    main.innerHTML = HomeScreen.render();
-
-}
+import HomeScreen from './srceens/HomeScreen.js';
+const router = () => {
+  const main = document.getElementById('main-container');
+  main.innerHTML = HomeScreen.render();
+};
 window.addEventListener('load', router);
