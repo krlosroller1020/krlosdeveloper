dc42315b0e97d5f77741836e0fe8d4911db34778
krlosLopez <krlosroller@gmail.com>
2021-05-31 21:34:45 -0500

    para brittany

--- frontend/src/srceens/ProductScreen.js
+++ frontend/src/srceens/ProductScreen.js
@@ -1,6 +1,20 @@
 const ProductScreen = {
   render: () => {
-    return `<div>ProductScreen</div>`;
+    return `<div>ProductScreen
+    
+    {
+      _id: '1',
+      name: 'Hiskywin Full Zip Running Shirts Thermal Workout',
+      category: 'Shirts',
+      image: '/images/pulsera (1).JPEG',
+      price: 59,
+      brand: 'Hiskywin',
+      rating: 4.5,
+      numReviews: 10,
+      countInStock: 6,
+    },
+    </div>`;
+    
   },
 };
 export default ProductScreen;
